dfd629705f214a690350edf1207c7196d27f4c5e
sunil <tsunil28k@gmail.com>
2018-07-24 15:57:51 +0530

    Add Notification feature.

---
+++ app/src/main/java/com/doozycod/nikache/fcm/NikacheFirebaseMessageService.java
@@ -0,0 +1,106 @@
+package com.doozycod.nikache.fcm;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.doozycod.nikache.MainActivity;
+import com.doozycod.nikache.R;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.Map;
+
+/**
+ * Created by sunilkumar on 24/07/18.
+ */
+
+/**
+ * Listens for squawk FCM messages both in the background and the foreground and responds
+ * appropriately
+ * depending on type of message
+ */
+public class NikacheFirebaseMessageService extends FirebaseMessagingService {
+
+    private static final int NOTIFICATION_MAX_CHARACTERS = 30;
+    private static String LOG_TAG = NikacheFirebaseMessageService.class.getSimpleName();
+
+    /**
+     * Called when message is received.
+     *
+     * @param remoteMessage Object representing the message received from Firebase Cloud Messaging
+     */
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // There are two types of messages data messages and notification messages. Data messages
+        // are handled
+        // here in onMessageReceived whether the app is in the foreground or background. Data
+        // messages are the type
+        // traditionally used with FCM. Notification messages are only received here in
+        // onMessageReceived when the app
+        // is in the foreground. When the app is in the background an automatically generated
+        // notification is displayed.
+        // When the user taps on the notification they are returned to the app. Messages
+        // containing both notification
+        // and data payloads are treated as notification messages. The Firebase console always
+        // sends notification
+        // messages. For more see: https://firebase.google.com/docs/cloud-messaging/concept-options\
+
+        // The Squawk server always sends just *data* messages, meaning that onMessageReceived when
+        // the app is both in the foreground AND the background
+
+        Log.d(LOG_TAG, "From: " + remoteMessage.getFrom());
+
+        // Check if message contains a data payload.
+
+        Map<String, String> data = remoteMessage.getData();
+
+        if (data.size() > 0) {
+            Log.d(LOG_TAG, "Message data payload: " + data);
+
+            // Send a notification that you got a new message
+            sendNotification(data);
+        }
+    }
+
+
+    /**
+     * Create and show a simple notification containing the received FCM message
+     *
+     */
+    private void sendNotification(Map<String, String> data) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        // Create the pending intent to launch the activity
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        String author = "hello";
+        String message = "Nikache";
+
+        // If the message is longer than the max number of characters we want in our
+        // notification, truncate it and add the unicode character for ellipsis
+        if (message.length() > NOTIFICATION_MAX_CHARACTERS) {
+            message = message.substring(0, NOTIFICATION_MAX_CHARACTERS) + "\u2026";
+        }
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.ic_duck)
+                .setContentTitle(String.format(getString(R.string.notification_message), author))
+                .setContentText(message)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+    }
+}
